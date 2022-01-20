692296f04a2fde8a595832c38c4655cdc0eb2d73
Shram_78 <ra00275@gmail.com>
2022-01-20 14:45:19 +0300

    plug clang - format

--- Source/ShooterShram/Private/SSGameModeBase.cpp
+++ Source/ShooterShram/Private/SSGameModeBase.cpp
@@ -1,5 +1,16 @@
 // ShooterShram Game. All Rights Reserved.
 
-
 #include "SSGameModeBase.h"
 
+void ASSGameModeBase::BeginPlay()
+{
+    int32 Variable = 0;
+    if (Variable < 10)
+        if (Variable < 10)
+        {
+            for (int32 i = 0; i < 10; i++)
+            {
+                UE_LOG(LogTemp, Display, TEXT("index: %i"), i);
+            }
+        }
+}
